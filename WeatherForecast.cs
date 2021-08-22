3a2f8e8164cd3a00d9c715e720cfb76b53176942
Alex Maylin <amaylin29@gmail.com>
2021-08-21 19:32:07 -0500

    nuevo campo de summary

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -12,6 +12,8 @@ namespace gitapi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public string Summary2 { get; set; }
         public string desc2 {get;set;}
 
         public string desc3 {get;set;}
