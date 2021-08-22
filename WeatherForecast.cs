c06be551d25d2d3318ffbd797a50519186447ebe
Alex Maylin <amaylin29@gmail.com>
2021-08-21 19:29:51 -0500

    campo nuevo de weatherforecast

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,6 +9,8 @@ namespace gitapi
 
         public int TemperatureC { get; set; }
 
+        public int TemperatureCnueva { get; set; }
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
